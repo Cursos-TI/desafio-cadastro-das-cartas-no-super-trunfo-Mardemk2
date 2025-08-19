e30af67827a5873bb2d7da1a4631063e5aef51f0
Mardemk2 <mardemk2@gmail.com>
2025-08-19 21:04:20 +0000

    novo

---
+++ cd02.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int main(){
+
+char Cidade[20], Codigo[20], Estado[20];
+int População, Turisticos;
+float Area, PIB;
+
+
+
+printf("Estado: Ceara\n");
+
+printf("Codigo da Cidade : A02\n");
+
+printf("Cidade: Fortaleza \n");
+
+printf("População: 5.000.000\n");
+
+printf("Principais pontos Turisticos: 100\n");
+
+printf("Area: 30.000\n");
+
+printf("PIB: 100.000.000\n");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
